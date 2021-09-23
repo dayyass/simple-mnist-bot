ceb65659dc12f8ef90865497b4cd38365a5b8cca
Dani El-Ayyass <dayyass@yandex.ru>
2021-09-23 16:25:12 +0300

    add model

--- mnist_convnet.py
+++ mnist_convnet.py
@@ -77,3 +77,9 @@ model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_spl
 score = model.evaluate(x_test, y_test, verbose=0)
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
+
+"""
+## Save the trained model
+"""
+
+model.save("model")
